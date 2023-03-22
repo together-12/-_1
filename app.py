99e37f77f6e958847b5e3d6824182f06b6761bb0
together-12 <127916760+together-12@users.noreply.github.com>
2023-03-22 11:49:41 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.template_folder = 'templates'
+
+messages = []
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        message = request.form.get('message')
+        messages.append(message)
+    return render_template('index.html', messages=messages)
+
+if __name__ == '__main__':
+    app.run(debug=True)
